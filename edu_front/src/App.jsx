4cf27b726aac432f8a1a89e764b9645ceafeaabc
franckpedron <franckpedrondev@gmail.com>
2025-09-23 11:47:40 +0200

    Init

---
+++ edu_front/src/App.jsx
@@ -0,0 +1,15 @@
+import {useState} from 'react'
+import './App.css'
+
+function App() {
+    const [count, setCount] = useState(0)
+
+    return (
+        <>
+            <h1>Bienvenue sur EDU DIP</h1>
+            <h2>Votre portail éducatif</h2>
+        </>
+    )
+}
+
+export default App
